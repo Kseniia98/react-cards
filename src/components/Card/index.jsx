004562dbed316ed1658e0ada95ed8a9f9e19e041
Kseniia98 <kseniia.plakhotnia@gmail.com>
2022-04-29 18:27:17 +0300

    Add cards

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Card.css";
+import book from "./img/book.png";
+import star from "./img/star.png";
+import branch from "./img/branch.png"
+
+function Card(props){
+  const { data } = props;
+  console.log(data);
+  return(
+    <div className="card">
+      <div className="cardHeader">
+        <img className="image" src={book} alt="book" />
+        <h3>{data.full_name}</h3>
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, ex!adipisicing elit. Eos, ex!
+      </div>
+      <div className="cardFooter">
+        <div>
+          {data.language === null ? "None" : data.language}
+        </div>
+        <div> 
+          <img className="image" src={star} alt="star" /> 
+          {data.stargazers_count}
+        </div>
+        <div> 
+          <img className="image" src={branch} alt="branch" /> 
+          {data.forks}
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default Card;
---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Cards.css";
+import Card from "../Card";
+
+class Cards extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.github.com/users/Kseniia98/repos')
+    .then((response) => {
+    return response.json();
+    })
+    .then((data) => {
+      this.setState((state) => ({
+        ...state,
+        list: data,
+      }))
+    })
+  }
+
+  render(){
+    return(
+      <div className="cards">
+        {this.state.list.map(data => <Card data={data}/>)}
+      </div>
+    )
+  }
+}
+
+export default Cards;
